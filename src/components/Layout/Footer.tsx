f793fefc7818c91d5c62048de7f164be9ebd0794
Johannes Grimm <jgrimm@hof-university.de>
2026-06-18 16:50:37 +0200

    feat: fixed ESLint no-unused-vars issue

--- src/components/Layout/Footer.tsx
+++ src/components/Layout/Footer.tsx
@@ -1,8 +1,5 @@
 import React from "react";
 import Link from "next/link";
-import { ExternalLink } from "lucide-react";
-
-
 
 // ─── Main Footer ──────────────────────────────────────────────────────────────
 
